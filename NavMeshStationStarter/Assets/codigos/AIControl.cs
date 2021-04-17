8be20a6f81df692deb699301708661b87be34d97
ViniSilva32 <78766084+ViniSilva32@users.noreply.github.com>
2021-04-16 22:19:11 -0300

    Aula IA NavMesh
    aula dia 16/04

---
+++ NavMeshStationStarter/Assets/codigos/AIControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIControl : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
