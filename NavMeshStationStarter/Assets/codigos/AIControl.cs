92da9f0690e71a360534cede3d754d2fec2b7e27
ViniSilva32 <78766084+ViniSilva32@users.noreply.github.com>
2021-04-19 20:09:17 -0300

    Aulas NavMesh
    Aula dia 19/04/2021

--- NavMeshStationStarter/Assets/codigos/AIControl.cs
+++ NavMeshStationStarter/Assets/codigos/AIControl.cs
@@ -5,16 +5,18 @@ using UnityEngine.AI;
 
 public class AIControl : MonoBehaviour
 {
+    //criação do Agente agent
     public NavMeshAgent agent;
     // Start is called before the first frame update
     void Start()
     {
+        //Agent interage com NavMeshAgent
         agent = this.GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Update vazio
     }
 }
--- NavMeshStationStarter/Assets/codigos/AgentManager.cs
+++ NavMeshStationStarter/Assets/codigos/AgentManager.cs
@@ -8,18 +8,23 @@ public class AgentManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //os agentes procuram os gameobjects com a tag "ai"
         agents = GameObject.FindGameObjectsWithTag("ai");
     }
 
     // Update is called once per frame
     void Update()
     {
+        // se o usuario clicar com o botão direito do mouse, faça uma ação
         if (Input.GetMouseButtonDown(0))
         {
+            //criação do hit
             RaycastHit hit;
+            //se o usuario clicar em algum lugar do mapa, tome uma ação
             if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
-            {
+            {   //se onde o player clicou estiver dentro da área definida
                 foreach (GameObject a in agents)
+                    // o player se move até o ponto onde foi clicado
                     a.GetComponent<AIControl>().agent.SetDestination(hit.point);
             }
         }
